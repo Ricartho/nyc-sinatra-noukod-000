82e0be8d51708e4b058917d2bc99b18d3f6ef6ca
Ricartho Franck <ricartho.franck@esih.edu>
2019-06-16 06:21:14 +0000

    done with new  form for figures

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,12 @@
 class FiguresController < ApplicationController
   # add controller methods
+  get '/figures' do
+
+  end
+
+  get '/figures/new' do
+    @titles = Title.all
+    @landmarks = Landmark.all
+    erb '/figures/new'.to_sym
+  end
 end
