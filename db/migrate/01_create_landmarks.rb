9a48f72427d94291a3b94c819d92c09cc2a42a44
Ricartho Franck <ricartho.franck@esih.edu>
2019-06-16 05:23:23 +0000

    done with models

--- db/migrate/01_create_landmarks.rb
+++ db/migrate/01_create_landmarks.rb
@@ -1,3 +1,24 @@
 class CreateLandmarks < ActiveRecord::Migration
-  raise 'Write CreateLandmarks migration here'
+  #raise 'Write CreateLandmarks migration here'
+  def change
+    create_table :figures do |el|
+      el.string :name
+    end
+
+    create_table :landmarks do |el|
+      el.belongs_to :figure, index: true
+      el.string :name
+      el.integer :year_completed
+    end
+
+    create_table :titles do |el|
+      el.string :name
+    end
+
+    create_table :figure_titles do |el|
+      el.belongs_to :title
+      el.belongs_to :figure
+    end
+
+  end
 end
