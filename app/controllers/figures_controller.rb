41015507f635f96f03f938cd78ba149abe525b04
Ricartho Franck <ricartho.franck@esih.edu>
2019-06-16 17:51:32 +0000

    done with figures actions

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,25 +1,38 @@
 class FiguresController < ApplicationController
   # add controller methods
-  get '/figures' do
+  get "/figures" do
     @figures = Figure.all
-    erb '/figures/index'.to_sym
+    erb "/figures/index".to_sym
   end
 
-  get '/figures/new' do
+  get "/figures/new" do
     @titles = Title.all
     @landmarks = Landmark.all
-    erb '/figures/new'.to_sym
+    erb "/figures/new".to_sym
   end
 
-  post '/figures' do
-    @figure = Figure.create(params[:figure])
-    @figure.landmarks << Landmark.create(params[:landmark]) if !params[:landmark].empty?
-    @figure.titles << Title.create(params[:title]) if !params[:title].empty?
-    redirect to "/figures/#{@figure.id}"
+  post "/figures" do
+    figure = Figure.create(params[:figure])
+    figure.landmarks << Landmark.create(params[:landmark]) if !params[:landmark].empty?
+    figure.titles << Title.create(params[:title]) if !params[:title].empty?
+    redirect to "/figures/#{figure.id}"
   end
 
-  get 'figures/:id' do
+  get "/figures/:id" do
     @figure = Figure.find(params[:id])
-    erb '/figures/show'.to_sym
+    erb "/figures/show".to_sym
+  end
+
+  get "/figures/:id/edit" do
+    @figure = Figure.find(params[:id])
+    erb "/figures/edit".to_sym
+  end
+
+  patch "/figures/:id" do
+    @figure = Figure.find_by_id(params[:id])
+    @figure.name = params[:figure][:name]
+    @figure.landmarks << Landmark.create(params[:landmark])
+    @figure.save
+    redirect to "/figures/#{@figure.id}"
   end
 end
